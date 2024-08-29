d85df5d7a79c718a791806b9dc36711593f59d40
Niwesh Sah <sahniwesh@gmail.com>
2024-08-29 21:56:48 +0530

    code updated

--- arm_teleop_2/src/final_4.py
+++ arm_teleop_2/src/final_4.py
@@ -15,7 +15,7 @@ class MyRobot:
     def __init__(self, Group_Name):
         # Initialize the moveit_commander and rospy node
         moveit_commander.roscpp_initialize(sys.argv)
-        rospy.init_node('node_set_redefined_pose', anonymous=True)
+        rospy.init_node('node_set_nearest_pose', anonymous=True)
 
         # Instantiate a RobotCommander object
         self._robot = moveit_commander.RobotCommander()
@@ -27,14 +27,13 @@ class MyRobot:
         # Instantiate a MoveGroupCommander Object
         self._group = moveit_commander.MoveGroupCommander(self._planning_group)
 
-
+        # Set planning time and number of attempts
         self._group.set_planning_time(10.0)
         self._group.set_num_planning_attempts(5)
 
-
         # Create action client for the Execute Trajectory action server
-        self._exectute_trajectory_client = actionlib.SimpleActionClient('execute_trajectory', moveit_msgs.msg.ExecuteTrajectoryAction)
-        self._exectute_trajectory_client.wait_for_server()
+        self._execute_trajectory_client = actionlib.SimpleActionClient('execute_trajectory', moveit_msgs.msg.ExecuteTrajectoryAction)
+        self._execute_trajectory_client.wait_for_server()
 
         # Get the planning frame, end effector link and the robot group names
         self._planning_frame = self._group.get_planning_frame()
@@ -46,36 +45,22 @@ class MyRobot:
         rospy.loginfo('\033[95m' + "Group Names: {}".format(self._group_names) + '\033[0m')
         rospy.loginfo('\033[95m' + " >>> MyRobot initialization is done." + '\033[0m')
 
-    def move_to_goal_pose(self, offset_x=0.1, offset_y=0.1, offset_z=0.1):
+    def move_to_nearest_pose(self, offset_x=0.1, offset_y=0.1, offset_z=0.1):
         # Get the current pose of the robot
         current_pose = self._group.get_current_pose().pose
 
         # Create a new goal pose slightly different from the current pose
-        pose_goal =  geometry_msgs.msg.Pose()
-        # pose_goal.header.frame_id = "base_link"  # Set the frame ID to "base_link"
-        # pose_goal.header.frame_id = "link_3"
-
+        pose_goal = geometry_msgs.msg.Pose()
         pose_goal.orientation = current_pose.orientation
-        # pose_goal.pose.orientation = current_pose.pose.orientation  # Corrected line
         pose_goal.position.x = current_pose.position.x + offset_x
         pose_goal.position.y = current_pose.position.y + offset_y
         pose_goal.position.z = current_pose.position.z + offset_z
 
-        # pose_goal.position.x = 0.0884937
-        # pose_goal.position.y = 0.344373
-        # pose_goal.position.z = 0.781796
-
-        # Set the goal pose and plan to it
+        # Set the goal pose and allow replanning if the initial plan fails
         self._group.set_pose_target(pose_goal)
-        plan = self._group.plan()
 
-
-        current_eef_link = self._group.get_end_effector_link()
-        rospy.loginfo(f"Current End Effector Link: {current_eef_link}")
-
-
+        # Plan to the nearest possible pose
         plan_success, plan, planning_time, error_code = self._group.plan()
-
         if plan_success:
             # Create a goal message object for the action server
             goal = moveit_msgs.msg.ExecuteTrajectoryGoal()
@@ -97,30 +82,17 @@ class MyRobot:
         rospy.loginfo('\033[95m' + "Object of class MyRobot Deleted." + '\033[0m')
 
 
-
 def main():
-
     # Create a new arm object from the MyRobot class
     arm = MyRobot("arm_body")
-    # hand = MyRobot("hand")
 
-    # Loop to move the arm and gripper to slightly different positions repeatedly
+    # Loop to move the arm to the nearest possible positions repeatedly
     while not rospy.is_shutdown():
-        arm.move_to_goal_pose(offset_x=0.0, offset_y=0.0, offset_z=0.01)
+        arm.move_to_nearest_pose(offset_x=0.0, offset_y=0.0, offset_z=0.01)
         rospy.sleep(3)
-
-        # arm.move_to_goal_pose(offset_x=0.0, offset_y=1, offset_z=0.0)
-        # rospy.sleep(2)
-
-        # # # hand.move_to_goal_pose(offset_x=0.05, offset_y=0.05, offset_z=0.05)
-        # # rospy.sleep(2)
-
-        # arm.move_to_goal_pose(offset_x=-0.1, offset_y=-0.1, offset_z=1.1)
-        # rospy.sleep(2)
 
     # Delete the arm object at the end of the code
     del arm
-    # del hand
 
 
 if __name__ == '__main__':
